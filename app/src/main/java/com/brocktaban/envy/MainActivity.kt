2ff2470005dfaee72882b132ac8d1b9867ce253a
Brock Taban <hello@brocktaban.com>
2019-04-12 14:10:35 -0400

    Designed Main Activity
    Added icons
    Added BottomSheetDialogFragment

--- app/src/main/java/com/brocktaban/envy/MainActivity.kt
+++ app/src/main/java/com/brocktaban/envy/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.brocktaban.envy
 
 import android.os.Bundle
+import com.google.android.material.bottomappbar.BottomAppBar
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.intentFor
 
-class MainActivity : _Main() {
+class MainActivity : _Main(), AnkoLogger, MainMenuModal.Listener {
+
+    override fun onMenuItemClicked(position: Int) {
+        when (position) {
+            0 -> {}
+        }
+    }
+
+    private var fabCenter = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +23,29 @@ class MainActivity : _Main() {
             startActivity(intentFor<Auth>())
         else
             setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bar)
+
+        fab.setOnClickListener {
+            if (fabCenter) {
+                bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                fab.setImageResource(R.drawable.ic_done_black_24dp)
+            } else {
+                bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                fab.setImageResource(R.drawable.ic_add_black_24dp)
+            }
+
+            fabCenter = !fabCenter
+        }
+
+        bar.setNavigationOnClickListener {
+            val menuList: Array<String> = arrayOf("Account", "Search", "Setting")
+            val menuIcons = ArrayList<Int>()
+
+            menuIcons.add(R.drawable.ic_person_black_24dp)
+            menuIcons.add(R.drawable.ic_search_black_24dp)
+            menuIcons.add(R.drawable.ic_settings_black_24dp)
+
+            MainMenuModal(menuList, menuIcons).show(supportFragmentManager, "main menu") }
     }
 }
--- app/src/main/java/com/brocktaban/envy/SignUp.kt
+++ app/src/main/java/com/brocktaban/envy/SignUp.kt
@@ -2,24 +2,109 @@ package com.brocktaban.envy
 
 
 import android.os.Bundle
+import android.util.Log.wtf
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.wtf
 
-/**
- * A simple [Fragment] subclass.
- */
 class SignUp : Fragment() {
+
+    private lateinit var mActivity: Auth
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
+        mActivity = activity as Auth
+        mAuth = FirebaseAuth.getInstance()
+
+
+        v.btnRegister.setOnClickListener { signUp(v) }
 
         return v
     }
 
+    private fun signUp(v: View) {
+        val displayName = v.etName.text.toString()
+        val email = v.etEmail.text.toString()
+        val password = v.etPassword.text.toString()
 
+        val nameLayout = v.ilName
+        val emailLayout = v.ilEmail
+        val passwordLayout = v.ilPassword
+
+        nameLayout.isErrorEnabled = false
+        emailLayout.isErrorEnabled = false
+        passwordLayout.isErrorEnabled = false
+
+        if (mActivity.isNullOrEmpty(displayName)) {
+            nameLayout.isErrorEnabled = true
+            nameLayout.error = "Name can't be empty"
+            return
+        }
+
+        if (displayName.length <= 3) {
+            nameLayout.isErrorEnabled = true
+            nameLayout.error = "Name is too short"
+            return
+        }
+
+        if (mActivity.isNullOrEmpty(email)) {
+            emailLayout.isErrorEnabled = true
+            emailLayout.error = "Email can't be empty"
+            return
+        }
+
+        if (mActivity.isNullOrEmpty(password)) {
+            passwordLayout.isErrorEnabled = true
+            passwordLayout.error = "Password can't be empty"
+            return
+        }
+
+        if (!mActivity.isEmailValid(email)) {
+            emailLayout.isErrorEnabled = true
+            emailLayout.error = "Email is not valid"
+            return
+        }
+
+        if (password.length <= 6) {
+            passwordLayout.isErrorEnabled = true
+            passwordLayout.error = "Password can't be less than 6 characters"
+            return
+        }
+
+        if (!v.checkbox.isChecked) {
+            v.main.longSnackbar("Please accept the terms and conditions", "Accept") {
+                v.checkbox.isChecked = true
+            }
+            return
+        }
+
+        v.btnRegister.isEnabled = false
+        v.progressBar.visibility = View.VISIBLE
+
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+
+                    v.btnRegister.isEnabled = true
+                    v.progressBar.visibility = View.GONE
+
+                    if (!task.isSuccessful) {
+                        wtf("Could not sign in", task.exception)
+                        v.main.longSnackbar(task.exception?.localizedMessage.toString())
+                        return@addOnCompleteListener
+                    }
+
+                    startActivity(context?.intentFor<MainActivity>())
+                }
+    }
 }
